9471bc1b669d8f9ae2241e4d9e722de7f1821b16
Michal Jenicek <michal.jenicek@strv.com>
2025-11-30 10:36:31 +0100

    Fix module version step.

--- codecheck.sh
+++ codecheck.sh
@@ -1,2 +1,11 @@
 #./gradlew clean spotlessApply detekt lintDebug testDebug
 ./gradlew clean spotlessApply detekt updateModuleVersions testDebug pixel4api35aospatdCheck
+
+
+# test whole group of gradle managed devices
+# ./gradlew ciGroupDebugAndroidTest
+
+# test single device
+#./gradlew pixel4api35aospatdCheck
+
+# ./gradlew clean pixel4api35aospatdCheck --rerun-tasks
--- codetest.sh
+++
@@ -1,7 +0,0 @@
-# test whole group of gradle managed devices
-# ./gradlew ciGroupDebugAndroidTest
-
-# test single device
-./gradlew pixel4api35aospatdCheck
-
-# ./gradlew clean pixel4api35aospatdCheck --rerun-tasks
