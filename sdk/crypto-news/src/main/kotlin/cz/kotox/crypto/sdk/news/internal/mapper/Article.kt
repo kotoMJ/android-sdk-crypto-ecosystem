17255710fc4f5afa864df9a86123d60f8fbe63c0
Michal Jenicek <jenicek.michal@gmail.com>
2025-11-30 11:22:41 +0100

    CodeGuard improvement (#14)
    * Fix nullability in the NewsApiResponseDTO
    * Add CI badge.
    * Add more fine grained CodeGuard steps with connection to the badge gist.

--- sdk/crypto-news/src/main/kotlin/cz/kotox/crypto/sdk/news/internal/mapper/Article.kt
+++ sdk/crypto-news/src/main/kotlin/cz/kotox/crypto/sdk/news/internal/mapper/Article.kt
@@ -16,12 +16,12 @@ import java.time.format.DateTimeParseException
  * Maps the entire [NewsApiResponseDTO] to a list of domain [Article] models.
  * It filters out unsuccessful responses and any articles that fail to parse.
  */
-public fun NewsApiResponseDTO.toDomainArticleList(): Either<SdkError, List<Article>> {
+public fun NewsApiResponseDTO.toDomainArticleList(requireTitle: Boolean): Either<SdkError, List<Article>> {
     // Only map successful responses that contain an article list
     return if (this.status != "ok" || this.articles == null) {
         Either.Error(ApiError.ResponseError(this.message ?: ""))
     } else {
-        Either.Value(this.articles.mapNotNull { it.toDomain() })
+        Either.Value(this.articles.mapNotNull { it.toDomain(requireTitle) })
     }
 }
 
@@ -31,7 +31,7 @@ public fun NewsApiResponseDTO.toDomainArticleList(): Either<SdkError, List<Artic
  * Returns null if essential data (like 'publishedAt') is invalid,
  * allowing the caller (e.g., `mapNotNull`) to filter it out.
  */
-private fun ArticleDTO.toDomain(): Article? {
+private fun ArticleDTO.toDomain(requireTitle: Boolean): Article? {
     val publishedAtInstant: Instant = try {
         Instant.parse(this.publishedAt)
     } catch (e: DateTimeParseException) {
@@ -40,10 +40,12 @@ private fun ArticleDTO.toDomain(): Article? {
         return null
     }
 
+    if (requireTitle && this.title == null) return null
+
     return Article(
         source = this.source.toDomain(), // Recursively map the nested object
         author = this.author,
-        title = this.title,
+        title = this.title ?: "",
         description = this.description,
         url = this.url,
         urlToImage = this.urlToImage,
