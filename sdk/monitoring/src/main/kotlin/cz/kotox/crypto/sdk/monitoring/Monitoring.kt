699a4912f19ce0cc7d185de74c7c9696a07b963b
Michal Jenicek <michal.jenicek@strv.com>
2026-03-22 23:08:03 +0100

    Remove redundant comments.

--- sdk/monitoring/src/main/kotlin/cz/kotox/crypto/sdk/monitoring/Monitoring.kt
+++ sdk/monitoring/src/main/kotlin/cz/kotox/crypto/sdk/monitoring/Monitoring.kt
@@ -8,20 +8,6 @@ public interface Monitoring {
         context = context,
     )
 
-//    /**
-//     * Fetches the current token for a network request
-//     *
-//     * @param uniqueRequestHash - is specifically designed to bind the integrity verdict to the content of your network request,
-//     * ensuring the data hasn't been tampered with in transit.
-//     * If you reuse the same hash (or the same token) for different requests,
-//     * a malicious actor could intercept a "good" token from a news request and try to reuse it to bypass security on a more sensitive action.
-//     */
-//    public suspend fun getFreshToken(uniqueRequestHash: String): SdkIntegrityToken?
-//
-//    public fun getIntegrityHash(content: String): String
-//
-//    public fun getSecurityHeader(): SdkSecurityHeader?
-
     public fun initMonitoring()
 
     public fun shutdown()
--- sdk/monitoring/src/main/kotlin/cz/kotox/crypto/sdk/monitoring/internal/MonitoringImpl.kt
+++ sdk/monitoring/src/main/kotlin/cz/kotox/crypto/sdk/monitoring/internal/MonitoringImpl.kt
@@ -11,20 +11,13 @@ import kotlinx.coroutines.SupervisorJob
 internal class MonitoringImpl(
     private val config: MonitoringConfig,
     private val sentryProvider: SentryProvider,
-//    private val dispatcher: CoroutineDispatcher,
 ) : Monitoring {
 
     private val job = SupervisorJob()
-//    private val scope = CoroutineScope(job + dispatcher)
 
     init {
         installLogger(config)
     }
-
-//    // Keep this lazy so we can init logger first.
-//    private val logger by lazy {
-//        SDKLogger.getLogger(MODULE_IDENTIFIER)
-//    }
 
     private fun installLogger(config: MonitoringConfig) {
         if (config.loggerCallback is SDKLoggerCallbackNoOp) {
@@ -33,11 +26,6 @@ internal class MonitoringImpl(
             SDKLogger.Companion.install(tag = MODULE_IDENTIFIER, loggerCallback = config.loggerCallback)
         }
     }
-
-//    override suspend fun getFreshToken(uniqueRequestHash: String): SdkIntegrityToken? =
-//        integrityProvider.getIntegrityToken(uniqueRequestHash)?.let { SdkIntegrityToken(it) }
-//
-//    override fun getIntegrityHash(content: String): String = content.toSha256Base64()
 
     override fun initMonitoring() {
         sentryProvider.initSentry()
